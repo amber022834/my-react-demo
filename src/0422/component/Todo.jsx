7ab2151d0acb99745f435487a2d228a086ac6bc9
amber022834 <sodagreen0228@gmail.com>
2025-04-22 11:22:16 +0800

    代辦事項

--- src/0422/component/Todo.jsx
+++ src/0422/component/Todo.jsx
@@ -1,7 +1,19 @@
-function Todo() {
+// function Todo(props) {
+//     return (
+//         <div className="todo">
+//             {/* 使用props接收元件屬性 */}
+//             <p>{props.todo}</p>
+//         </div>
+//     )
+// }
+// export default Todo
+
+
+function Todo({todo}) {
     return (
         <div className="todo">
-            <p>停車費</p>
+            {/* 使用props接收元件屬性 */}
+            <p>{todo.content}</p>
         </div>
     )
 }
--- src/0422/component/TodoWrapper.jsx
+++ src/0422/component/TodoWrapper.jsx
@@ -1,13 +1,25 @@
+import { useState } from "react"
 import CreateForm from "./CreateForm"
 import Todo from "./Todo"
 
 function TodoWrapper() {
-     return (
+    // 有多個todo因此用陣列存放物件資料
+    const [todos, setTodos] = useState([
+        { content: '停車費', id: Math.random() },
+        { content: '對發票', id: Math.random() },
+    ])
+
+    return (
         <div className="wrapper">
             <h1>代辦事項</h1>
-            <CreateForm/>
-            <Todo/>
+            <CreateForm />
+            {
+                todos.map((todo) => {
+                    return <Todo todo={todo} key={todo.id}/>
+                })
+            }
+
         </div>
-     )
+    )
 }
 export default TodoWrapper

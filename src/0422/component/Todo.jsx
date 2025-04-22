f78cfd7b11a5ca480b3ccb4b750d8e4a96249a8c
amber022834 <sodagreen0228@gmail.com>
2025-04-22 15:19:53 +0800

    react

--- src/0422/component/Todo.jsx
+++ src/0422/component/Todo.jsx
@@ -8,18 +8,23 @@
 // }
 // export default Todo
 
-import { MdDelete } from "react-icons/md";
-import { MdEdit } from "react-icons/md";
+import { MdDelete, MdEdit } from "react-icons/md";
 
-function Todo({ todo }) {
+
+function Todo({ todo, delTodo, toggleCompleted }) {
     return (
-        <div className="todo">
+        //使用三元運算子
+        //條件式?成立:不成立
+        <div className={`todo ${todo.isCompleted ? 'completed' : ''}`}>
             {/* 使用props接收元件屬性 */}
-            <p>{todo.content}</p>
+            <p onClick={() => { toggleCompleted(todo.id) }}>{todo.content}</p>
             {/* 修改 */}
             <div>
-                <MdEdit style={{ cursor: 'pointer', color:"grey"}} />
-                < MdDelete style={{ cursor: 'pointer', marginleft:"5px" ,color:"grey"}} />
+                <MdEdit style={{ cursor: 'pointer', color: "grey" }} />
+                < MdDelete
+                    //刪除按鈕觸發delTodo方法
+                    onClick={() => { delTodo(todo.id) }}
+                    style={{ cursor: 'pointer', marginleft: "5px", color: "grey" }} />
             </div>
 
         </div>
--- src/0422/component/TodoWrapper.jsx
+++ src/0422/component/TodoWrapper.jsx
@@ -5,26 +5,52 @@ import Todo from "./Todo"
 function TodoWrapper() {
     // 有多個todo因此用陣列存放物件資料
     const [todos, setTodos] = useState([
-        { content: '停車費', id: Math.random() },
-        { content: '對發票', id: Math.random() },
+        { content: '停車費', id: Math.random(), isCompleted: false },
+        { content: '對發票', id: Math.random(), isCompleted: false },
     ])
 
     //建立加入新的todo內容(函式)
-    const addTodo=(newContent)=>{
+    const addTodo = (newContent) => {
         //其餘運算子
-        setTodos([...todos,{content:newContent, id: Math.random()}])
-
+        setTodos([...todos, { content: newContent, id: Math.random(), isCompleted: false }])
     }
 
+    //建立刪除函式
+    //傳入被刪除的id
+    const delTodo = (id) => {
+        setTodos(todos.filter((todo) => {
+            //如果被刪除id不等於原來的id就要保留
+            return todo.id !== id
+        }))
+    }
+
+    //切換是否被點擊，更改於原本isCompleted狀態
+    const toggleCompleted = (id) => {
+        setTodos(todos.map((todo) => {
+            // if(todo.id==id){
+            //     return {...todo, isCompleted:!todo.isCompleted }
+            // }else{
+            //     return todo
+            // }
+
+            // 三源運算子({條件成立，更改狀態} 括號外為else的狀態)
+            return todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+        }))
+    }
 
     return (
         <div className="wrapper">
             <h1>代辦事項</h1>
             {/* addTodo式屬性，後面括號內是function */}
-            <CreateForm addTodo={addTodo}/>
+            <CreateForm addTodo={addTodo} />
             {
                 todos.map((todo) => {
-                    return <Todo todo={todo} key={todo.id}/>
+                    return <Todo
+                        todo={todo}
+                        key={todo.id}
+                        delTodo={delTodo}
+                        toggleCompleted={toggleCompleted}
+                    />
                 })
             }
 

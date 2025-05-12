8c91a9834124413d2c128b7dc1ee847fe607a94e
amber022834 <sodagreen0228@gmail.com>
2025-05-12 09:59:32 +0800

    登入表單

---
+++ src/0512/App.jsx
@@ -0,0 +1,72 @@
+
+import { div } from "motion/react-client";
+import { useState, createContext, useContext } from "react";
+
+function App() {
+
+    //建立共用環境
+    const UserContext = createContext({});
+
+    //建立使用者變數
+    const [username, setUsername]=useState('')
+
+    //建立登入紐控制
+    const [isLogin,setIsLogin]=useState(false);
+
+    //建立登入元件
+    const LoginForm=()=>{
+        const {username,setUsername,setIsLogin}=useContext(UserContext);
+
+
+
+        return(
+            <>
+            <label htmlFor="username">使用者名稱</label>
+            <input 
+            type="text" 
+            id="username"
+            placeholder="請輸入使用者名稱"
+            value={username}
+            onChange={e=>setUsername(e.target.value)}/>
+
+            <button type="button" onClick={()=>{setIsLogin(true)}}>登入 </button>
+            </>
+        )
+
+    }
+
+
+    //登入後的元件
+    const Greeting =()=>{
+        //與登入元件為同一階層，所以可以透過useContext功能從userContext取得username
+        const {username}=useContext(UserContext);
+        return(
+            <div>
+                Hi，{username}
+            </div>
+        )
+
+    }
+
+
+
+    return (
+        <>
+          <h1>useContext</h1>
+          <hr style={{marginBottom:"50px"}}/>
+          <UserContext.Provider value={{username,setUsername,setIsLogin}}>
+
+            {
+                isLogin?<Greeting/>:<LoginForm/>
+            }
+
+          </UserContext.Provider>
+
+        </>
+          
+
+       
+    )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './0429/App-faq'
+import App from './0512/App'
 // import './index.css'
 // import './style.css'
 import { BrowserRouter } from 'react-router-dom'

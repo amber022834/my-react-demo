4a4324df2ab218a60e4a16a84cd4f29b126ac312
amber022834 <sodagreen0228@gmail.com>
2025-05-13 13:03:29 +0800

    localstorage

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './0513/App3'
+import App from './0513/App4'
 // import './index.css'
 // import './style.css'
 import { BrowserRouter } from 'react-router-dom'

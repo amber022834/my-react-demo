a39ca660fa5ff46043a3a620e743f5d681a7e9d5
amber022834 <sodagreen0228@gmail.com>
2025-05-27 09:49:54 +0800

    購物車

--- src/0527/App.jsx
+++ src/0527/App.jsx
@@ -1,8 +1,9 @@
-
+import Reducer from "./component/reducer"
 export default function App(){
     return(
         <>
         <h1>useReducer</h1>
+        <Reducer/>
         </>
     )
 }
--- src/0527/component/Reducer.jsx
+++ src/0527/component/Reducer.jsx
@@ -1,9 +1,29 @@
+import { useReducer } from "react"
+
 export default function Reducer() {
+    //設定預設值
+    const initialState = 0;
+
+    //改變state函式
+    const reducer = (state, action) => {
+        switch (action) {
+            case 'add':
+                return state + 1;
+
+            case 'sub':
+                return state-1
+            default:
+                return state;
+        }
+    }
+
+    const [count, dispatch] = useReducer(reducer, initialState);
     return (
         <>
             <div>
-                <p>count:{1}</p>
-                
+                <p>count:{count}</p>
+                <button onClick={() => dispatch('add')}>+</button>
+                <button onClick={() => dispatch('sub')}>-</button>
             </div>
         </>
     )
